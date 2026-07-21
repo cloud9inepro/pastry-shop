2c75fcd9840f3ea496f908f20a03effd7e38a4ad
Gift <giftemmanuel84@gmail.com>
2026-07-21 19:33:04 +0100

    refactor: clean up Map component code and improve marker icon handling

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,20 +1,46 @@
+import React from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+} from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
-export const   Map = () => {
-  const position = [4.8156, 70.0499]; // Example coordinates near Port Harcourt/Nigeria
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
+export const Map = () => {
+  const position = [4.8156, 7.0499];
 
   return (
-    <MapContainer center={position} style={{ height: "200px", width: "100%" }}>
+    <MapContainer
+      center={position}
+      zoom={13}
+      style={{
+        height: "200px",
+        width: "100%",
+        borderRadius: "12px",
+      }}
+    >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        attribution="&copy; OpenStreetMap contributors"
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+
       <Marker position={position}>
-        <Popup>Your Map Placement!</Popup>
+        <Popup>Port Harcourt, Nigeria</Popup>
       </Marker>
     </MapContainer>
   );
-}
+};
